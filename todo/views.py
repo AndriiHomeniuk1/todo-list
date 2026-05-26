de6f1023d18ac7e6a75cc28a9a9f174cd259ba02
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-05-26 13:25:53 +0300

    feat(todo): implement task and tag CRUD views with routing

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from .forms import TaskForm
+from .models import Tag, Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    tasks = Task.objects.prefetch_related("tags")
+
+    context = {
+        "tasks": tasks
+    }
+
+    return render(request, "todo/index.html", context)
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = ["name"]
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo/todo_confirm_delete.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"delete_type": "tag"})
+        return context
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "todo/todo_confirm_delete.html"
+    success_url = reverse_lazy("todo:index")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"delete_type": "task"})
+        return context
+
+
+def toggle_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect("todo:index")

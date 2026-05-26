3405ee434621333302d18e4735cbfe4cd5009a49
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-05-26 13:27:56 +0300

    feat(todo): add TaskForm with custom widgets

---
+++ todo/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = [
+            "content",
+            "deadline",
+            "tags",
+        ]
+
+        widgets = {
+            "deadline": forms.DateTimeInput(
+                attrs={"type": "datetime-local", "class": "form-control"}
+            ),
+            "tags": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check"}
+            ),
+
+        }
